fb048a3618940318e59d97d0e463b0aa3d77616c
wokron <stringcatwok@gmail.com>
2024-02-27 20:35:50 +0800

    add test_ode

---
+++ neurode/tests/test_ode.py
@@ -0,0 +1,33 @@
+from ..ode.ode import ODE
+from ..calc.equations import Equations, Derivation as d
+from ..calc.calc_node import Placeholder
+import numpy as np
+
+
+def test_ode_calc():
+    x = Placeholder("x")
+    y = Placeholder("y")
+    alpha = Placeholder("alpha")
+    beta = Placeholder("beta")
+    gamma = Placeholder("gamma")
+    delta = Placeholder("delta")
+
+    ode = ODE(
+        Equations(
+            [
+                d(x) == alpha * x - beta * x * y,
+                d(y) == gamma * x * y - delta * y,
+            ]
+        )
+    )
+
+    assert ode.params == {"alpha": 0, "beta": 0, "gamma": 0, "delta": 0}
+
+    ode.params.update({"alpha": 2, "beta": 0.02, "gamma": 0.0002, "delta": 0.8})
+
+    t = np.linspace(0, 10)
+    val = ode.calc([5000, 100], t)
+
+    assert len(t) == len(val)
+    assert (3000 <= val[:, 0]).all() and (val[:, 0] <= 5000).all()
+    assert (80 <= val[:, 1]).all() and (val[:, 1] <= 120).all()
