4eac61fab551d6cb3cad2ed7ab09d6f98fd0438a
wokron <stringcatwok@gmail.com>
2024-02-28 17:12:28 +0800

    add test_tensor_calculation

--- neurode/tests/test_calc_node.py
+++ neurode/tests/test_calc_node.py
@@ -1,10 +1,45 @@
 from ..calc.calc_node import Placeholder
+import torch
 
 
 def test_basic_calculation():
     a = Placeholder("a")
     b = Placeholder("b")
     data = {"a": 1, "b": 2}
+
+    # a + b = 3
+    val = a + b
+    assert val(data) == 3
+
+    # a + 1 = 2
+    val = a + 1
+    assert val(data) == 2
+
+    # a + (-1) = 0
+    val = a + (-1)
+    assert val(data) == 0
+
+    # 1 + a = 2
+    val = 1 + a
+    assert val(data) == 2
+
+    # a - b = -1
+    val = a - b
+    assert val(data) == -1
+
+    # a * b = 2
+    val = a * b
+    assert val(data) == 2
+
+    # a / b = 1/2
+    val = a / b
+    assert val(data) == 1 / 2
+
+
+def test_tensor_calculation():
+    a = Placeholder("a")
+    b = Placeholder("b")
+    data = {"a": torch.tensor(1), "b": torch.tensor(2)}
 
     # a + b = 3
     val = a + b
