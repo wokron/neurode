e9ff3332654eb8ec69c41565b84ebb3054cb766d
wokron <stringcatwok@gmail.com>
2024-02-29 21:51:13 +0800

    update example

--- example.py
+++ example.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 from neurode import ODE, derivate as d, var
 
 x1 = var("x1")
@@ -6,18 +9,18 @@ r1 = var("r1")
 r2 = var("r2")
 a1 = var("a1")
 a2 = var("a2")
-n1 = var("n1")
-n2 = var("n2")
+in1 = var("in1")
+in2 = var("in2")
 
 ode = ODE(
     [
-        d(x1) == r1 * x1 * (1 - x1 / n1) - a1 * x1 * x2,
-        d(x2) == r2 * x2 * (1 - x2 / n2) - a2 * x1 * x2,
+        d(x1) == r1 * x1 * (1 - x1 * in1) - a1 * x1 * x2,
+        d(x2) == r2 * x2 * (1 - x2 * in2) - a2 * x1 * x2,
     ]
 )
 
-t = [0, 10, 15, 30, 36, 40, 42]
-y = [
+t = np.array([0, 10, 15, 30, 36, 40, 42])
+y = np.array([
     [100, 150],
     [165, 283],
     [197, 290],
@@ -25,16 +28,23 @@ y = [
     [305, 269],
     [318, 266],
     [324, 264],
-]
+])
 
 ode.params = {
     "r1": 0.1,
     "r2": 0.1,
     "a1": 0.001,
     "a2": 0.001,
-    "n1": 1000,
-    "n2": 1000,
+    "in1": 0.001,
+    "in2": 0.001,
 }
-ode.fit(t, y, verbose=True)
-
+ode.fit(t, y, verbose=True, epoches=5000)
 print(ode.params)
+
+t2 = np.linspace(0, 50)
+y2 = ode.calc(t2, y[0])
+
+plt.scatter(t, y[:, 0])
+plt.scatter(t, y[:, 1])
+plt.plot(t2, y2)
+plt.show()
